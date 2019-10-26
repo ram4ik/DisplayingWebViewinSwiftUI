114961f27e6ccc6d48f44c51d64d285527fe72db
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-26 22:26:18 +0300

    init

---
+++ DisplayingWebViewinSwiftUI/WebView.swift
@@ -0,0 +1,31 @@
+//
+//  WebView.swift
+//  DisplayingWebViewinSwiftUI
+//
+//  Created by ramil on 26.10.2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    var url: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: self.url) else {
+            return WKWebView()
+        }
+        
+        let request = URLRequest(url: url)
+        let wkWebview = WKWebView()
+        wkWebview.load(request)
+        return wkWebview
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebView>) {
+        
+    }
+}
